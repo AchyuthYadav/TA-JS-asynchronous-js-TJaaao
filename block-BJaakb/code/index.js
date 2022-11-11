5adc1ed88b868e08bae147c9f2ce69e34b1a85c8
achyuth yadav <achyuthyadav@hmail.com>
2022-11-11 12:17:43 +0530

    assignment

--- block-BJaakb/code/index.js
+++ block-BJaakb/code/index.js
@@ -2,7 +2,7 @@ function fetch(url){
     return  new Promise ((resolve, reject) => {
         let xhr = new HMLHttpReqest();
         xhr.open('GET',url);
-        xhr.onload = ()=> resolve(JSON.parse(fKingXHR.response));
+        xhr.onload = ()=> resolve(JSON.parse(xhr.response));
         xhr.onerror = ()=> reject("something wrong");
         xhr.send();
     })
